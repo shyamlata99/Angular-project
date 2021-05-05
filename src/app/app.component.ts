519a547eddb2bd12beb1fec8a398faaaa894b868
Shyamlata Dubey <shyamlata65@gmail.com>
2021-05-05 20:51:58 +0530

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angularFirstApp';
+
+  public empName:string;
+  public empId: number;
+  public  data=[];
+
+  submit(){
+    let entry={Name:this.empName, Id:this.empId};
+     this.empName="" ;
+     this.empId=null;
+     this.data.push(entry);
+  }
+
+  delete(remove){
+    this.data.splice(remove ,1);
+  }
 }
+
